73f7d42b3889e9887ba20993f4494dfbf907f7d0
franciscode09 <francisduchere@gmail.com>
2026-02-04 11:50:32 +0000

    Add files via upload

---
+++ db.js
@@ -0,0 +1,12 @@
+export const users = [
+    {
+      id: "1",
+      email: "francisduchere@gmail.com",
+      password: "1234",
+      balance: 1000,
+      transactions: []
+    }
+  ];
+  
+  export const otps = {};
+  
